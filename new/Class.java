f62a5cf85fe0ef24ebc6ac0763e3099a190fc273
juhi.sharma <juhi.sharma@TVMATP403905D.ad.infosys.com>
2016-12-13 17:21:53 +0530

    pull

---
+++ new/Class.java
@@ -0,0 +1,11 @@
+package newpack;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("abx");
+System.out.println("ab");
+	}
+
+}
